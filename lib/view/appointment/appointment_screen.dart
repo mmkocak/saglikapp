59122e4c4dfc0dd3e82e5a4386950676d07e4ba6
mmkocak <muhammetlocak774@gmail.com>
2024-04-10 01:29:28 +0300

    Appointment sayfasına başladım

--- lib/view/appointment/appointment_screen.dart
+++ lib/view/appointment/appointment_screen.dart
@@ -1,16 +1,96 @@
 import 'package:flutter/material.dart';
 
 class AppointmentScreen extends StatelessWidget {
- AppointmentScreen({ Key? key }) : super(key: key);
-List imgs = [
-  "doctor1.jpg",
-  "doctor2.jpg",
-  "doctor3.jpg",
-  "doctor4.jpg",
-];
+  AppointmentScreen({Key? key}) : super(key: key);
+  List imgs = [
+    "doctor1.jpg",
+    "doctor2.jpg",
+    "doctor3.jpg",
+    "doctor4.jpg",
+  ];
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF7165d6),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Stack(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                  
+                   Padding(
+                    padding: const  EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const CircleAvatar(
+                          radius: 35,
+                          backgroundImage:
+                              AssetImage("assets/images/doctor1.jpg"),
+                        ),
+                        const SizedBox(height: 15),
+                        const Text(
+                          "Dr. Doktor Adı",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 5),
+                       const  Text(
+                          "Terapist",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 5,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(10),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
