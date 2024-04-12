aeb2853a0e3ade0f6c05a745aa282427f7aab18f
mmkocak <muhammetlocak774@gmail.com>
2024-04-12 23:45:44 +0300

    Appointment güncelleme

--- lib/view/appointment/appointment_screen.dart
+++ lib/view/appointment/appointment_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AppointmentScreen extends StatelessWidget {
   AppointmentScreen({Key? key}) : super(key: key);
@@ -118,60 +119,109 @@ class AppointmentScreen extends StatelessWidget {
             Container(
               height: MediaQuery.sizeOf(context).height,
               width: double.infinity,
-              padding:const EdgeInsets.only(
+              padding: const EdgeInsets.only(
                 top: 20,
                 left: 15,
               ),
               decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10)
-                )
-              ),
-              child:  Column(
-
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10))),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
-                
                 children: [
-                  const Text("Doktor Hakkında",
-                  style:   TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500
-                     ),
+                  const Text(
+                    "Doktor Hakkında",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                   ),
-                  const SizedBox(height: 5,),
-                  const Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.", 
-                  style: TextStyle(fontSize: 16, color: Colors.black54),
+                  const SizedBox(
+                    height: 5,
                   ),
-                  const SizedBox(height: 10,),
+                  const Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                    style: TextStyle(fontSize: 16, color: Colors.black54),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
                   Row(
                     children: [
-                     const Text("Yorumlar", 
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500
+                      const Text(
+                        "Yorumlar",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w500),
                       ),
+                      const SizedBox(
+                        width: 10,
                       ),
-                     const  SizedBox(width: 10,),
-                     const Icon(Icons.star, color: Colors.amber,),
-                     const Text("4.9", style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16,
-                      ),),
-                     const SizedBox(width: 5,),
-                     const Text("123", style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16,
-                        color: Color(0xFF7565d6),
-                      ),),
-                     const Spacer(),
-                      TextButton(onPressed: (){}, child: Text("Tümünü Görüntüle"))
+                      const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      const Text(
+                        "4.9",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      const Text(
+                        "123",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Color(0xFF7565d6),
+                        ),
+                      ),
+                      const Spacer(),
+                      TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Tümünü Görüntüle",
+                            style: TextStyle(
+                                color: Color(0xFF7565d6),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16),
+                          ))
                     ],
                   ),
+                  SizedBox(height: 160,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    itemBuilder: ((context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 2
+                          ),
+                        ],
+
+                      ),
+                      child: SizedBox(
+                        width: MediaQuery.sizeOf(context).width / 1.4,
+                        child: Column(
+                          children: [],
+                        ),
+                      ),
+                    );
+                  })),
+                  ),
+                  
                 ],
-                ),
+              ),
             ),
           ],
         ),
